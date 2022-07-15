d0b3f9595015771393d87a64acbc7541f4c82bdc
Ilya Kublin <raka699@gmail.com>
2022-07-15 17:41:41 +0300

    Started tabs selection menu

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlazorGameInstance.Model;
+using GodlessBoard.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace GodlessBoard.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly MyDbContext _dbContext;
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        public Game TestGenericGame { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, MyDbContext myDbContext)
         {
+            _dbContext = myDbContext;
             _logger = logger;
         }
 
         public void OnGet()
         {
-
+            TestGenericGame = Newtonsoft.Json.JsonConvert.DeserializeObject<Game>(_dbContext.Games.Where(x => x.Id == 3).Single().JsonRepresentation);
         }
     }
 }
