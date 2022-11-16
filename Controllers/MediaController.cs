c35e046f1ab7e08b385447d080ac9eb7188c44da
Ilya Kublin <raka699@gmail.com>
2022-11-16 16:37:01 +0300

    edited remove and upload

--- Controllers/MediaController.cs
+++ Controllers/MediaController.cs
@@ -1,15 +1,17 @@
-﻿using GodlessBoard.Data;
+﻿using GameModel;
+using GodlessBoard.Data;
 using GodlessBoard.Models;
 using GodlessBoard.Services;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Net.Mime;
 using System.Security.Claims;
 
 namespace GodlessBoard.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]/[action]/{id}")]
     public class MediaController : ControllerBase
     {
         private readonly MyDbContext _dbContext;
@@ -24,6 +26,25 @@ namespace GodlessBoard.Controllers
             _mediaUploadRouter = mediaUploadRouter;
         }
 
+        private async Task<User> AuthorizeRequestAsync()
+        {
+            if (!Request.Headers.Any(x => x.Key == "gboard_signin_token"))
+                return null;
+            var token = Request.Headers.First(x => x.Key == "gboard_signin_token");
+
+            if (!_jwtHandler.ValidateToken(token.Value))
+                return null;
+
+            var userName = _jwtHandler.GetClaims(token.Value).First(x => x.Type == ClaimTypes.Email).Value;
+
+            if(! await _dbContext.Users.AnyAsync())
+            {
+                return null;
+            }
+
+            return await _dbContext.Users.FirstAsync(x => x.UserName == userName);
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -31,38 +52,56 @@ namespace GodlessBoard.Controllers
 
         }
 
-        [HttpPost]
-        public async Task<IActionResult> UploadImage(byte [] file)
+        [HttpGet]
+        public async Task<IActionResult> Remove(long id)
         {
-            if (!Request.Headers.Any(x => x.Key == "gboard_signin_token"))
-                return Unauthorized();
-            
-            var token = Request.Headers.First(x => x.Key == "gboard_signin_token");
-            
-            if (!_jwtHandler.ValidateToken(token.Value))
+            var user = await AuthorizeRequestAsync();
+
+            if (user == null)
                 return Unauthorized();
 
-            var userName = _jwtHandler.GetClaims(token.Value).First(x => x.Type == ClaimTypes.Email).Value;
-
-
-            if (_mediaUploadRouter.CheckExistance(file, userName, file.FileName))
-                return Ok();
-            var user = _dbContext.Users.First(x => x.UserName == userName);
             
-            var media = await _mediaUploadRouter.UploadMediaAsync(ms.ToArray(), userName, file.FileName);
-            media.Owner = user;
-            media.OwnerId = user.Id;
-            media.Weight = file.
-            _dbContext.Media.Add(new Media() {
-                Owner = user,
-                OwnerId = user.Id,
-                Weight = 
+            if(! await _dbContext.Media.AnyAsync(x => x.Id == id && x.OwnerId == user.Id))
+            {
+                return BadRequest();
+            }
 
-            });
+            var media = await _dbContext.Media.FirstAsync(x => x.Id == id);
 
+            await _mediaUploadRouter.DeleteAsync(media);
+
+            _dbContext.Media.Remove(media);
+
+            await _dbContext.SaveChangesAsync();
+
+            
 
             return Ok();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(byte [] bytes, string json)
+        {
+            var user = await AuthorizeRequestAsync();
+
+            if (user == null)
+                return Unauthorized();
+
+           var file = Newtonsoft.Json.JsonConvert.DeserializeObject<MediaFile>(json);
+
+            if (_mediaUploadRouter.CheckExistance(bytes, user.UserName, file.UserDisplayName))
+                return Ok();
+            
+            var media = await _mediaUploadRouter.UploadMediaAsync(bytes, user.UserName, file.UserDisplayName);
+            media.Owner = user;
+            media.OwnerId = user.Id;
+            media.Weight = file.Size;
+            _dbContext.Media.Add(media);
+            await _dbContext.SaveChangesAsync();
+            
+            return Ok();
+        }
+
         
     }
 }
